e8a8d00c5309b3dfdf3d1bfb1e45a654c2011cd7
kostyadudnyk <dudnyk2000@gmail.com>
2021-03-24 20:15:04 +0200

    v0.1.4.4 New hash and state system

--- core/init.php
+++ core/init.php
@@ -11,39 +11,12 @@
 	}
 	spl_autoload_register('classAutoLoad');
 
-	function getLang($lang) {
-        if ($lang == "ru" || $lang == "uk" || $lang == "kz" || $lang == "be" || is_null($lang)) {
-            $lang = "ru";
-        }else{
-            $lang = "en";
-        }
-	    return $lang;
-    }
-
-    function decl($int, $expr){
-        settype($int, "integer");
-        $count = $int % 100;
-        if ($count >= 5 && $count <= 20) {
-            $result = $int." ".$expr['2'];
-        } else {
-            $count = $count % 10;
-            if ($count == 1) {
-                $result = $int." ".$expr['0'];
-            } elseif ($count >= 2 && $count <= 4) {
-                $result = $int." ".$expr['1'];
-            } else {
-                $result = $int." ".$expr['2'];
-            }
-        }
-        return $result;
-    }
-
     // Дебаг любых данных в файл
     function dumpData($data) {
         file_put_contents(__DIR__ . '/dumpdata.txt', print_r($data, true));
     }
 
-    include 'core/config.php'; // Основной конфиг
+    include 'config.php'; // Основной конфиг
     include 'lang/'.LANG.'.php';
 
 	//Подключаем Базу Данных
