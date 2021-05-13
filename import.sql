ae63a36bcbcb0ea193d5f9113b7eb8bc9133dcae
kostyadudnyk <dudnyk2000@gmail.com>
2021-05-13 16:36:38 +0300

    -Актуализировал import.sql

--- import.sql
+++ import.sql
@@ -1,57 +1,117 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Мар 23 2021 г., 18:23
--- Версия сервера: 5.7.32
--- Версия PHP: 5.4.16
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `db_747`
---
-
--- --------------------------------------------------------
-
 --
 -- Структура таблицы `categories`
 --
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` int(11) NOT NULL,
+    `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films`
+--
+
+CREATE TABLE IF NOT EXISTS `films` (
+    `id` int(11) NOT NULL,
+    `hash` varchar(255) DEFAULT NULL,
+    `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    `photo` varchar(255) DEFAULT NULL,
+    `trailer` varchar(255) DEFAULT NULL,
+    `video` varchar(255) DEFAULT NULL,
+    `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `year` varchar(255) DEFAULT NULL,
+    `categories` text,
+    `private_msg_id` int(11) DEFAULT NULL,
+    `public_msg_id` int(11) DEFAULT NULL,
+    `public2_msg_id` int(11) DEFAULT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `_accounts` (
+    `id` int(11) NOT NULL,
+    `status` int(11) NOT NULL DEFAULT '0',
+    `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+    `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+    `chat_id` int(11) NOT NULL,
+    `lang` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `banned` int(1) NOT NULL DEFAULT '0',
+    `admin` int(1) NOT NULL DEFAULT '0',
+    `menu` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `last_action` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `_dialogs`
+--
+
+CREATE TABLE IF NOT EXISTS `_dialogs` (
+    `id` int(11) NOT NULL,
+    `hash` varchar(255) NOT NULL,
+    `chat_id` int(11) NOT NULL,
+    `menu` varchar(64) DEFAULT NULL,
+    `data` int(11) DEFAULT NULL,
+    `data2` varchar(255) DEFAULT NULL,
+    `page` int(11) DEFAULT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `_reactions`
+--
+
+CREATE TABLE IF NOT EXISTS `_reactions` (
+    `id` int(11) NOT NULL,
+    `item_id` int(11) NOT NULL,
+    `chat_id` varchar(64) NOT NULL,
+    `react` int(1) NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `categories`
---
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
+ALTER TABLE `films`
+    ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
+ALTER TABLE `_accounts`
+    ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT для таблицы `categories`
---
+ALTER TABLE `_dialogs`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `_reactions`
+    ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `films`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `_accounts`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `_dialogs`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `_reactions`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
