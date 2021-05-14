f966e4339b5c69d6f4c894a7c3d81de341cb8a07
kostyadudnyk <dudnyk2000@gmail.com>
2021-05-14 17:07:28 +0300

    -Исправления

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 
     // Настройки подключения к DB MySQL
     define('DB_SERVER','localhost'); // Сервер БД
-    define('DB_NAME','filmbot'); // Название БД
-    define('DB_PASSWORD','H5c5Z3a6'); // Пароль БД
-    define('DB_USERNAME','filmbot'); // Пользователь БД
+    define('DB_NAME',''); // Название БД
+    define('DB_PASSWORD',''); // Пароль БД
+    define('DB_USERNAME',''); // Пользователь БД
 
     // Настройки бота
-    define('BOT_TOKEN','1718673256:AAEBwFJOV3bIQz3ENGkQqnzp8pCL7-H3SnI'); // API Token бота
+    define('BOT_TOKEN','1-H3SnI'); // API Token бота
     define('LANG','ru'); // Стандартный язык
     define('PER_PAGE', 3 ); // Кол-во строчек на страницу
     define('PER_PAGE_COL', 2 ); // Кнопок колонок на страницу
@@ -16,15 +16,12 @@
 
     //Настройки чатов
     // * Бот должен быть добавлен в канал как администратор
-    define('PRIVATECHATID', -1001165241074 ); // ID/Link приватного канала для публикаций. (false/chat_id)
-    define('PUBLICCHATID', "@tesctchanneks"); // ID/Link публичного канала для публикаций. (false/chat_id/chatlink(@demochannel))
+    define('PRIVATECHATID', - ); // ID/Link приватного канала для публикаций. (false/chat_id)
+    define('PUBLICCHATID', "@"); // ID/Link публичного канала для публикаций. (false/chat_id/chatlink(@demochannel))
 
     //Возможные сюжеты
     $scripts = ['admin','main'];
 
     // Chat_id => "Role"
     $employers = [
-        920538735 => "Admin",
-        684000481 => "Admin",
-        517019519 => "Admin"
     ];
